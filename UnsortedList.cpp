fa1216df028e2a72781e6b5503365e7dbde14d23
DrErickson <77122859+DrErickson@users.noreply.github.com>
2021-01-25 16:17:38 -0800

    Implemented the constructor, MakeEmpty, IsFull, GetLength functions.  Added stubs to the UnsortedList.cpp file.

--- UnsortedList.cpp
+++ UnsortedList.cpp
@@ -1,1 +1,41 @@
 
+#include "UnsortedList.h"
+
+UnsortedList::UnsortedList() {
+    length = 0;
+    currentPos = -1;  // 0
+
+}
+
+void UnsortedList::MakeEmpty() {
+    length = 0;
+}
+
+bool UnsortedList::IsFull() const {
+    return length == MAX_ITEMS;
+}
+
+int UnsortedList::GetLength() const {
+    return length;
+}
+
+bool UnsortedList::Contains(int someItem) {
+    return false;
+}
+
+void UnsortedList::PutItem(int item) {
+
+}
+
+void UnsortedList::DeleteItem(int item) {
+
+}
+
+void UnsortedList::ResetIterator() {
+
+}
+
+int UnsortedList::GetNextItem() {
+    return 0;
+}
+
